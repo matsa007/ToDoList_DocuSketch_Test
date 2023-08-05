acd2a0994a09ac79bc11ea5f5da984271ea0c46e
matsa_007 <serg.arbitran@gmail.com>
2023-08-05 12:38:29 +0300

    Blank TableView with custom cell added

---
+++ ToDoList_DocuSketch_Test/ListOfTasksTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListOfTasksTableViewCell.swift
+//  ToDoList_DocuSketch_Test
+//
+//  Created by Сергей Матвеенко on 5.08.23.
+//
+
+import UIKit
+
+final class ListOfTasksTableViewCell: UITableViewCell {
+    
+    // MARK: - Lifecycle
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+    }
+
+}
--- ToDoList_DocuSketch_Test/ListOfTasksViewController.swift
+++ ToDoList_DocuSketch_Test/ListOfTasksViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 import SnapKit
 
-class ListOfTasksViewController: UIViewController {
+final class ListOfTasksViewController: UIViewController {
+    
+    // MARK: - GUI
+    
+    private lazy var listofTasksTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .green
+        return tableView
+    }()
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .lightGray
+        self.setTableView()
+        self.addSubviews()
+        self.setConstraints()
+    }
+    
+    private func setTableView() {
+        self.listofTasksTableView.delegate = self
+        self.listofTasksTableView.dataSource = self
+        self.listofTasksTableView.register(ListOfTasksTableViewCell.self, forCellReuseIdentifier: "list_cell")
+    }
+    
+    private func addSubviews() {
+        self.view.addSubview(self.listofTasksTableView)
+    }
+    
+    private func setConstraints() {
+        self.listofTasksTableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+extension ListOfTasksViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "list_cell", for: indexPath) as? ListOfTasksTableViewCell else { return UITableViewCell() }
+        return cell
     }
 }
 
