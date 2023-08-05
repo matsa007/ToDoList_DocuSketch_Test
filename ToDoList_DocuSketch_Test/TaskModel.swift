d364936e2ef9e5a1b6be81b408ab86cbd9e7a414
matsa_007 <serg.arbitran@gmail.com>
2023-08-05 12:42:41 +0300

    Task model added

---
+++ ToDoList_DocuSketch_Test/TaskModel.swift
@@ -0,0 +1,14 @@
+//
+//  TaskModel.swift
+//  ToDoList_DocuSketch_Test
+//
+//  Created by Сергей Матвеенко on 5.08.23.
+//
+
+import Foundation
+
+struct TaskModel {
+    var taskTitle: String
+    var taskDescription: String?
+    var priorityLevel: String?
+}
