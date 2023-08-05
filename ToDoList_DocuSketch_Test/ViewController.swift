e106635223d4413440f5eddf4ca4f7f7d6f36055
matsa_007 <serg.arbitran@gmail.com>
2023-08-05 12:02:29 +0300

    no message

---
+++ ToDoList_DocuSketch_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList_DocuSketch_Test
+//
+//  Created by Сергей Матвеенко on 5.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
